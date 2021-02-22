41f703bc5ce713533c28e2e117935c8cc1209148
Yeseo Kim <YKim@lcs.on.ca>
2021-02-22 12:15:08 -0500

    Initiated ISpeakTXTMSG app.

---
+++ ISpeakTXTMSGApp/ISpeakTXTMSGAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ISpeakTXTMSGAppApp.swift
+//  ISpeakTXTMSGApp
+//
+//  Created by Yeseo Kim on 2021-02-22.
+//
+
+import SwiftUI
+
+@main
+struct ISpeakTXTMSGAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
